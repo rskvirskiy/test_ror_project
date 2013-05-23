b9629bb0f0f26d4796505f02dc318a2e41f44ba7
Rskvirskiy <rskvirskiy@gmail.com>
2013-05-23 18:30:29 +0300

    emty project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestRorProject::Application.config.secret_token = '38ce8952815f48b35478545226dbd9bdb444fb34d70f3c3881049b575ce25425171dd025b892befa72cc7c4dc3c30cd2d1e92ecea9a294167cb3ec8d277346d4'
