0f2aa789cb73511d1b2d873ca45f5d39b26efe77
rskvirskiy <rskvirskiy@gmail.com>
2013-05-28 10:55:57 +0300

    add password and password_confirmation to model User, add methode for save authenticate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,16 @@
 class User < ActiveRecord::Base
-  attr_accessible :address, :birthday, :city, :country, :email, :fullname, :login, :state, :zip
+  attr_accessible :address, :birthday, :city, :country, :email, :fullname, :login, :state, :zip, :password, :password_confirmation
+  has_secure_password
 
   
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates: :email, presence: trueformat: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 
-  validates: :login, presence: true, length: { maximum: 30 }, uniqueness: true
-  validates: :fullname, presence: true, length: { maximum: 50 }
+  validates :login, presence: true, length: { maximum: 30 }, uniqueness: true
+  validates :fullname, presence: true, length: { maximum: 50 }
+
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
 
   before_save { |user| user.email = email.downcase }
 end
