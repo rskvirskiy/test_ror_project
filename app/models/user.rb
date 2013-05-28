34523989b074b4469ed7114670a0d4bab2485ff8
rskvirskiy <rskvirskiy@gmail.com>
2013-05-28 10:25:10 +0300

    add user model without password, add validation of unique email and login, create migration for unique email

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+  attr_accessible :address, :birthday, :city, :country, :email, :fullname, :login, :state, :zip
+
+  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates: :email, presence: trueformat: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+
+  validates: :login, presence: true, length: { maximum: 30 }, uniqueness: true
+  validates: :fullname, presence: true, length: { maximum: 50 }
+end
---
+++ db/migrate/20130528071224_create_users.rb
@@ -0,0 +1,17 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :login
+      t.string :fullname
+      t.time :birthday
+      t.string :email
+      t.string :address
+      t.string :city
+      t.string :state
+      t.string :country
+      t.string :zip
+
+      t.timestamps
+    end
+  end
+end
