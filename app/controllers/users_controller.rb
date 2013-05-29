2f3a593d283efb32863a6f8329cba5fe94e461ab
rskvirskiy <rskvirskiy@gmail.com>
2013-05-29 22:15:18 +0300

    simple working registration with validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,16 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 	end
 
+	def create
+		@user = User.new(params[:user])
+		if @user.save
+			redirect_to @user
+		else
+			render 'new'
+		end
+	end
+
 	def new
+		@user = User.new
 	end
 end
