5fe9a356af77b15a29ba33942515d4bc160883f9
rskvirskiy <rskvirskiy@gmail.com>
2013-06-14 19:27:22 +0300

    bugs with twitter authentication fixed

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,18 +1,30 @@
 class AuthenticationsController < ApplicationController
   def index
-    @authentications = Authentication.all
+    @authentications = current_user.authentications if current_user
   end
 
   def create
     auth = request.env["omniauth.auth"]
-    current_user.authentications.find_or_create_by_provider_and_uid(auth['provider'], auth['uid'])
-    flash[:notice] = "Authentication successfull"
-    redirect_to authentications_url
+    if @user=User.find_by_login(auth[:info][:nickname]) #if we have this user
+      sign_in @user
+    else #if we don't have, we'll create this user
+      @user = User.new(login: auth[:info][:nickname].to_s, fullname: auth[:info][:name].to_s, email: "example@twitter.com", password: "foobar", password_confirmation: "foobar" );
+      if @user.save
+        sign_in @user
+      else
+        flash[:error]="Unknown error with authentication"
+        redirect_to '/signin'
+      end
+    end
+      current_user.authentications.find_or_create_by_provider_and_uid(auth[:provider], auth[:uid])
+      flash[:notice] = "Authentication successfull"
+      redirect_to @user
   end
 
   def destroy
-    @authentication = Authentication.find(params[:id])
+    @authentication = current_user.authentication.find(params[:id])
     @authentication.destroy
-    redirect_to authentications_url, :notice => "Successfully destroyed authentication."
+    flash[:notice] = "Successfully destroyed authentication."
+    redirect_to authentications_url
   end
 end
