a3bf7ae9fc3adc5b37f890db9b8d5ccbb9d097e3
skvirskiy <rskvirskiy@gmail.com>
2013-07-24 22:38:13 +0300

    added form for new  comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,6 +3,11 @@ class CommentsController < ApplicationController
 	before_filter :correct_user, only: :destroy
 
 	def create
+		@comment = Micropost.find(params[:comment][:micropost_id]).comments.create(params[:comment])
+		if @comment.save
+			flash[:success] = "Comment created!"
+			redirect_to @comment.micropost.user
+		end
 	end
 
 	def destroy
